695c1c38bb6d4abeb6aead78918ab05e22e05b94
Bruno Guimaraes <devgmrs@gmail.com>
2022-09-08 18:52:46 -0300

    :sparkles:	feat: Updating the project's base files and adding eslit and prettier

---
+++ src/componentes/Botao/index.jsx
@@ -0,0 +1,9 @@
+import './botao.css'
+
+function Botao({texto}) {
+    return <button type='button' className="botao">
+        {texto}
+    </button>
+}
+
+export default Botao
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,17 +1,31 @@
-import Colaborador from '../Colaborador'
-import './time.css'
+/* eslint-disable react/jsx-filename-extension */
+import Colaborador from '../Colaborador';
+import './time.css';
 
-const Time = ({ time, colaboradores }) => {
-    return (
-
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: time.corPrimaria }}>
-            <h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
-            <div className='colaboradores'>
-                {colaboradores.map((colaborador, indice) => <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.corSecundaria} />)}
-            </div>
-        </section>
-
-    )
+function Time({ time, colaboradores }) {
+	return (
+		colaboradores.length > 0 && (
+			<section
+				className="time"
+				style={{
+					backgroundImage: 'url(/imagens/fundo.png)',
+					backgroundColor: time.corPrimaria,
+				}}
+			>
+				<h3 style={{ borderColor: time.corSecundaria }}>{time.nome}</h3>
+				<div className="colaboradores">
+					{colaboradores.map((colaborador, indice) => (
+						<Colaborador
+							// eslint-disable-next-line react/no-array-index-key
+							key={indice}
+							colaborador={colaborador}
+							corDeFundo={time.corSecundaria}
+						/>
+					))}
+				</div>
+			</section>
+		)
+	);
 }
 
-export default Time
+export default Time;
